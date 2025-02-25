2d4c70ec64c583079045bf67fe79b2214e517288
Benson Michael Owen <lordkz19@gmail.com>
2025-02-25 19:44:52 +0100

    get request for create

--- controllers/external/createRoom.js
+++ controllers/external/createRoom.js
@@ -4,17 +4,17 @@ const { v4: uuidv4 } = require('uuid');
 
 const createRoomButton = async (req,res) =>{
     try{
-        const {room} = req.body
+        const {rid} = req.params
         const secret = uuidv4();
      
-        db.query("SELECT * FROM channels WHERE channel = ? ", [room], async(err, data) =>{
+        db.query("SELECT * FROM channels WHERE channel = ? ", [rid], async(err, data) =>{
             if(err){
                 console.log(err)
             }
             if(data[0]){
                 return res.json({success:"Meeting Already Exists"})
             }else{
-                db.query("INSERT INTO channels SET ?", [{title:room,channel_secret:secret, channel:room }], async (err, inserted) =>{
+                db.query("INSERT INTO channels SET ?", [{title:rid,channel_secret:secret, channel:rid }], async (err, inserted) =>{
                         if(err){
                             console.log(err)
                             return res.json({error:err})
